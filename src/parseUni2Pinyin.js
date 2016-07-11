71fe0aa3f58199c9ce754580049d86ff3a954b9b
Cody Jenkins <iknowcss@gmail.com>
2016-07-11 14:49:20 +1000

    Initial character to pinyin conversion

--- src/parseUni2Pinyin.js
+++ src/parseUni2Pinyin.js
@@ -3,18 +3,27 @@ import {readFileLines} from './util/file';
 
 const COMMENT_REGEXP = /^\s*#/;
 
-export default (filePath) => {
-  const entries = [];
+export default class Uni2PinyinMap {
+  constructor(entries) {
+    this.entries = entries;
+  }
+  
+  getPinyin(hanzi) {
+    return this.entries[hanzi];
+  }
+}
+
+Uni2PinyinMap.init = (filePath) => {
+  const entries = {};
   return new Promise((fulfill, reject) => {
     readFileLines(filePath, (err, line) => {
-      if (line.EOF) return fulfill(entries);
+      if (line.EOF) return fulfill(new Uni2PinyinMap(entries));
       if (err) return reject(err);
       if (COMMENT_REGEXP.test(line)) return;
 
       const [utfString, ...prons] = line.split('\t');
       const hanzi = String.fromCharCode(parseInt(utfString, 16));
-      entries.push([hanzi, prons]);
+      entries[hanzi] = prons;
     });
   });
-}
-
+};
--- src/parseUni2Pinyin.test.js
+++ src/parseUni2Pinyin.test.js
@@ -1,13 +1,29 @@
 import path from 'path';
-import consume from './parseUni2Pinyin';
+import Uni2PinyinMap from './parseUni2Pinyin';
 
-const dataPath = path.join(__dirname, '../../../data/Uni2Pinyin');
+const DATA_PATH = path.join(__dirname, '../../../data/Uni2Pinyin');
 
-describe('parseUniToPinyin', () => {
-  it('does a thing', () => {
-    return consume(dataPath)
-      .then((entries) => {
-        console.log(entries.length)
-      });
+describe('Uni2PinyinMap', () => {
+  describe('init', () => {
+    it('builds a new map from provided file', () => {
+      return Uni2PinyinMap.init(DATA_PATH)
+        .then(map => {
+          expect(map).to.be.instanceof(Uni2PinyinMap);
+        });
+    });
+  });
+
+  describe('mapping', () => {
+    let y2pMap;
+
+    before(() => {
+      return Uni2PinyinMap.init(DATA_PATH)
+        .then(map => y2pMap = map);
+    });
+
+    it('gets the possible pronunciations of a character', () => {
+      expect(y2pMap.getPinyin('我')).to.eql(['wo3']);
+      expect(y2pMap.getPinyin('乐')).to.eql(['le4', 'yue4']);
+    });
   });
 });
